b2a55b7037989e066ac09031869741c8eaca5a93
MilesChou <jangconan@gmail.com>
2022-10-01 10:46:00 +0800

    day15

--- app/Http/Controllers/Auth/HydraCallback.php
+++ app/Http/Controllers/Auth/HydraCallback.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use Ory\Hydra\Client\Api\AdminApi;
 use Ory\Hydra\Client\Api\PublicApi;
 
 class HydraCallback
 {
-    public function __invoke(Request $request, PublicApi $hydra)
+    public function __invoke(Request $request, PublicApi $hydra, AdminApi $admin)
     {
         $error = $request->input('error');
 
@@ -31,7 +34,20 @@ class HydraCallback
             return response('請求 Token 失敗');
         }
 
+        Log::debug('Token Response: ', json_decode((string)$tokenResponse, true));
+
         dump(json_decode((string)$tokenResponse, true));
+
+        $userinfoEndpoint = $hydra->getConfig()->getHost() . '/userinfo';
+
+        $userInfo = Http::withToken($tokenResponse->getAccessToken())
+            ->get($userinfoEndpoint);
+
+        Log::debug('User info: ', $userInfo->json());
+
+        $introspectToken = $admin->introspectOAuth2Token($tokenResponse->getAccessToken());
+
+        Log::debug('Token Introspection: ', json_decode((string)$introspectToken, true));
 
         return response('拿到身分驗證回應了');
     }
